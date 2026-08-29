520d2be1d96c5b111dfd201a9f2daef9b63c75e1
shivam <shivamdalal0105@gmail.com>
2026-08-29 16:56:41 +0530

    Polymorphism

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We are learnig Polymorphism");
+        }
+    }
+}
