bc952dcb878fae93f624934138be63f14bf40d4e
shivam <shivamdalal0105@gmail.com>
2026-08-29 14:28:35 +0530

    OOPS 1st program

---
+++ OOPS2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OOPS2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Namaste World");
+            Console.WriteLine("  ");
+        }
+    }
+}
