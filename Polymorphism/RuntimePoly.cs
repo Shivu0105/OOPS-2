575a7b4bb7786cdf7388ae6c6e1e0197b20c2de6
shivam <shivamdalal0105@gmail.com>
2026-08-30 22:15:35 +0530

    Commit

---
+++ Polymorphism/RuntimePoly.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    // virtual & override keyword 
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Animal Making sound..!");
+        }
+    }
+
+    class Dog : Animal
+    {
+    public override void MakeSound()
+        {
+            Console.WriteLine("Barking");
+        }
+    }
+}
