d9308c898804c055b041e427ad00b16d08cb1956
shivam <shivamdalal0105@gmail.com>
2026-08-30 13:24:17 +0530

    Learning 2 Inheritance

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalSpace
+{
+    class Animal                        // Parent/Base/Super Class
+    {
+        public void Sleep()
+        {
+            Console.WriteLine("Sleeping");
+        }
+        public void Running()
+        {
+            Console.WriteLine("Running");
+        }
+    }
+
+    class Dog : Animal                  // Child/Sub Class
+    {
+        public void Bark()
+        {
+            Console.WriteLine("bhow bhow...");
+        }
+    }
+    class Cat : Animal
+    {
+        public void Meaw()
+        {
+            Console.WriteLine("Meaw...");
+        }
+    }
+
+    class Tiger : Animal
+    {
+        public void Roar()
+        {
+            Console.WriteLine("Roar...");
+        }
+    }
+}
---
+++ Inheritance/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSpace
+{
+    internal class Car
+    {
+       public void Engine()
+        {
+            Console.WriteLine("Implemented...!");
+        }
+    }
+}
