0a72ab626bb41a43beaffed7f46e8f3298b2fb5e
shivam <shivamdalal0105@gmail.com>
2026-08-30 15:42:56 +0530

    Polymorphism_Div

---
+++ Polymorphism_Div/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Polymorphism_Div
+{
+    class Demo
+    {
+        public int Div(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Can't divide by zero");
+                return 0;
+            }
+            return a / b;
+
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo d = new Demo();
+
+            Console.WriteLine("Enter a Value");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter b Value");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int result = d.Div(a, b);
+            Console.WriteLine("Result " + result);
+
+        }
+    }
+}
