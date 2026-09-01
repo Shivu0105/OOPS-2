3112c793bec8de82920eae63e18146818bc4a413
shivam <shivamdalal0105@gmail.com>
2026-09-01 16:17:09 +0530

    Poly 01/09/26

--- Polymorphism/obj/Debug/net10.0/Polymorphism.AssemblyInfo.cs
+++ Polymorphism/obj/Debug/net10.0/Polymorphism.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Polymorphism")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+575a7b4bb7786cdf7388ae6c6e1e0197b20c2de6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+122789dbfd576d3a7f2caa817784d21ec22ab6db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Polymorphism")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Polymorphism")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
