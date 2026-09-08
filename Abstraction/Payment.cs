eb671f6cb6236070351b95901ffee259d404f7f6
shivam <shivamdalal0105@gmail.com>
2026-09-09 00:16:40 +0530

    Abstraction

---
+++ Abstraction/Payment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraction
+{
+    public abstract class Payment
+    {
+        public abstract void Pay(decimal amount);
+    }
+
+    public class CreditCardPayment : Payment
+    {
+        public override void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid Rs{amount} using Credit Card.");
+        }
+    }
+
+    public class UPIPayment : Payment
+    {
+        public override void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid Rs{amount} using UPI.");
+        }
+    }
+
+    public class NetBanking : Payment
+    {
+        public override void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid Rs{amount} using Netbanking.");
+        }
+    }
+
+    public class ByCash : Payment
+    {
+        public override void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid Rs{amount} using by Cash.");
+        }
+    }
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Payment P1 = new CreditCardPayment();
+            P1.Pay(10000);
+            Console.WriteLine();
+
+            Payment P2 = new UPIPayment();
+            P2.Pay(500);
+            Console.WriteLine();
+
+            Payment P3 = new NetBanking();
+            P3.Pay(50000);
+            Console.WriteLine();
+
+            Payment P4 = new ByCash();
+            P4.Pay(300);
+        }
+    }
+}
