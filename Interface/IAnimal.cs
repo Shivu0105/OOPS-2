88257f6daed8474fef4c838e5d738337b13dbfbe
shivam <shivamdalal0105@gmail.com>
2026-09-09 01:30:23 +0530

    Interface.

---
+++ Interface/IAnimal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    public interface IAnimal
+    {
+        void MakeSound();  // Every animal must have a method called MakeSound().
+        int Legs { get; set; }//Every animal must also have a property called Legs.
+    }                          // (get;) -> Means you can read the value.
+                               // (set;) -> Means you can change the value.
+
+    public class Dog : IAnimal  // Dog implements the interface.
+    {
+        public int Legs { get; set; } = 4;
+
+        public void MakeSound()  // Dog now provides its own version of MakeSound().
+        {
+            Console.WriteLine("Dog Bark!"); // Whenever Dog makes a sound:
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        public int Legs { get; set; } = 4;
+        public void MakeSound()
+        {
+            Console.WriteLine("Cat Meow!");
+        }
+    }
+
+    public class Monkey : IAnimal
+    {
+        public int Legs { get; set; } = 2;
+        public int Hands { get; set; } = 2;
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Monkey Screaming!");
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimal myDog = new Dog();
+            myDog.MakeSound();
+            Console.WriteLine($"Dogs having {myDog.Legs} Legs");
+            Console.WriteLine();
+
+            IAnimal myCat = new Cat();
+            myCat.MakeSound();
+            Console.WriteLine($"Cat having {myCat.Legs} Legs");
+            Console.WriteLine();
+
+            Monkey myMonkey = new Monkey();
+            myMonkey.MakeSound();
+            Console.WriteLine($"Monkey having {myMonkey.Hands} Hands");
+            Console.WriteLine($"Monkey having {myMonkey.Legs} Legs");
+
+        }
+    }
+}
